07db7b536a497e3858e8be9c8942e13f201e60b9
Raquel <ragontru25@gmail.com>
2021-05-31 10:19:04 +0200

    desde modificar

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,190 @@
+import { Component, OnInit } from '@angular/core';
+import { AlertController, ModalController, ToastController } from '@ionic/angular';
+import { ApiServiceProvider } from 'src/providers/api-service/api-service';
+import { EditarAlumnoPage } from '../editar-alumno/editar-alumno.page';
+import { Alumno } from '../modelo/Alumno';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage implements OnInit {
+
+  private alumnos = new Array<Alumno>();
+
+  constructor(private apiService: ApiServiceProvider,
+    public alertController: AlertController,
+    public modalController: ModalController,
+    public toastController: ToastController) {
+  }
+
+  ngOnInit(): void {
+    this.apiService.getAlumnos()
+      .then((alumnos: Alumno[]) => {
+        this.alumnos = alumnos;
+        console.log(this.alumnos);
+      })
+      .catch((error: string) => {
+        console.log(error);
+      });
+  }
+
+  eliminarAlumno(indice: number) {
+    this.apiService.eliminarAlumno(this.alumnos[indice].id)
+      .then((correcto: boolean) => {
+        console.log("Borrado correcto del alumno con indice: " + indice);
+        this.alumnos.splice(indice, 1);
+      })
+      .catch((error: string) => {
+        console.log("Error al borrar: " + error);
+      });
+  }
+  /*
+    async modificarAlumno(indice: number) {
+      let alumno = this.alumnos[indice];
+      const alert = await this.alertController.create({
+        header: 'Modificar',
+        inputs: [
+          {
+            name: 'first_name',
+            type: 'text',
+            value: alumno.first_name,
+            placeholder: 'first_name'
+          },
+          {
+            name: 'last_name',
+            type: 'text',
+            id: 'last_name',
+            value: alumno.last_name,
+            placeholder: 'last_name'
+          },
+          {
+            name: 'email',
+            id: 'email',
+            type: 'text',
+            value: alumno.email,
+            placeholder: 'email'
+          },
+          {
+            name: 'gender',
+            id: 'gender',
+            type: 'text',
+            value: alumno.gender,
+            placeholder: 'gender'
+          },
+          {
+            name: 'avatar',
+            value: alumno.avatar,
+            type: 'url',
+            placeholder: 'avatar'
+          },
+          {
+            name: 'address',
+            value: alumno.address,
+            type: 'text',
+            placeholder: 'address'
+          },
+          {
+            name: 'city',
+            value: alumno.city,
+            type: 'text',
+            placeholder: 'city'
+          }
+        ],
+        buttons: [
+          {
+            text: 'Cancel',
+            role: 'cancel',
+            cssClass: 'secondary',
+            handler: () => {
+              console.log('Confirm Cancel');
+            }
+          }, {
+            text: 'Ok',
+            handler: (data) => {
+              console.log(data);
+              var alumnoModificado: Alumno = new Alumno(alumno.id,
+                data['first_name'],
+                data['last_name'],
+                data['email'],
+                data['gender'],
+                data['avatar'],
+                data['address'],
+                data['city'],
+                data['postalCode']);
+              this.apiService.modificarAlumno(alumno.id, alumnoModificado)
+                .then((alumno: Alumno) => {
+                  this.alumnos[indice] = alumno;
+                })
+                .catch((error: string) => {
+                  console.log(error);
+                });
+              console.log('Confirm Ok');
+            }
+          }
+        ]
+      });
+  
+      await alert.present();
+    }*/
+
+  async modificarAlumno(indice: number) {
+    const modal = await this.modalController.create({
+      component: EditarAlumnoPage,
+      componentProps: {
+        'alumnoJson': JSON.stringify(this.alumnos[indice])
+      }
+    });
+
+    modal.onDidDismiss().then((data) => {
+      if (data['data'] != null) {
+        let alumnoJSON = JSON.parse(data['data']);
+        let alumnoModificado: Alumno = Alumno.createFromJsonObject(alumnoJSON);
+        this.apiService.modificarAlumno(alumnoModificado.id, alumnoModificado)  //se hace PUT a la API
+          .then((alumno: Alumno) => {
+            this.alumnos[indice] = alumno;  //si se ha modificado en la api se actualiza en la lista
+          })
+          .catch((error: string) => {
+            console.log(error);
+          });
+      }
+    });
+
+    return await modal.present();
+  }
+
+  async nuevoAlumno() {
+    const modal = await this.modalController.create({
+      component: EditarAlumnoPage,
+      componentProps: {
+        'alumnoJson': JSON.stringify(new Alumno(-1,
+          "", "", "", "", "", "", "", ""))
+      }
+    });
+
+    modal.onDidDismiss().then((data) => {
+      if (data['data'] != null) {
+        let alumnoJSON = JSON.parse(data['data']);
+        let alumnoNuevo: Alumno = Alumno.createFromJsonObject(alumnoJSON);
+        this.apiService.insertarAlumno(alumnoNuevo)  //se hace POST a la API
+          .then((alumno: Alumno) => {
+            this.alumnos.push(alumno);  //si se ha insertado en la api se añade en la lista
+          })
+          .catch((error: string) => {
+            this.presentToast("Error al insertar: " + error);
+          });
+      }
+    });
+    return await modal.present();
+  }
+
+  async presentToast(message: string) {
+    const toast = await this.toastController.create({
+      message: message,
+      duration: 2000
+    });
+    toast.present();
+  }
+
+}
---
+++ src/providers/api-service/api-service.ts
@@ -0,0 +1,107 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Alumno } from 'src/app/modelo/Alumno';
+
+@Injectable()
+export class ApiServiceProvider {
+
+    private URL = "http://localhost:3000";
+
+    constructor(public http: HttpClient) {
+    }
+
+    getAlumnos(): Promise<Alumno[]> {
+        let promise = new Promise<Alumno[]>((resolve, reject) => {
+            this.http.get(this.URL + "/alumnos").toPromise()
+                .then((data: any) => {
+                    let alumnos = new Array<Alumno>();
+                    data.forEach(alumnoJson => {
+                        let alumno = Alumno.createFromJsonObject(alumnoJson);
+                        alumnos.push(alumno);
+                    });
+                    resolve(alumnos);
+                })
+                .catch((error: Error) => {
+                    reject(error.message);
+                });
+        });
+        return promise;
+    }
+
+    eliminarAlumno(id: number): Promise<Boolean> {
+        let promise = new Promise<Boolean>((resolve, reject) => {
+            this.http.delete(this.URL + "/alumnos/" + id).toPromise().then(
+                (data: any) => { // Success
+                    console.log(data)
+                    resolve(true);
+                }
+            )
+                .catch((error: Error) => {
+                    console.log(error.message);
+                    reject(error.message);
+                });
+        });
+        return promise;
+    }
+
+    modificarAlumno(idAlumno: number, nuevosDatosAlumno: Alumno): Promise<Alumno> {
+        let promise = new Promise<Alumno>((resolve, reject) => {
+            var header = { "headers": { "Content-Type": "application/json" } };
+            let datos = nuevosDatosAlumno.getJsonObject();
+            this.http.put(this.URL + "/alumnos/" + idAlumno,
+                JSON.stringify(datos),
+                header).toPromise().then(
+                    (data: any) => { // Success
+                        let alumno: Alumno;
+                        alumno = Alumno.createFromJsonObject(data);
+                        resolve(alumno);
+                    }
+                )
+                .catch((error: Error) => {
+                    reject(error.message);
+                });
+        });
+        return promise;
+
+    }
+
+    insertarAlumno(datosNuevoAlumno: Alumno): Promise<Alumno> {
+
+        let promise = new Promise<Alumno>((resolve, reject) => {
+
+            var header = { "headers": { "Content-Type": "application/json" } };
+
+            let datos = datosNuevoAlumno.getJsonObject();
+
+            delete datos.id; //cuando se hace un post no paso el id. El id es asignado por el servidor. Quito el atributo del objeto json
+
+            this.http.post(this.URL + "/alumnos/",
+
+                JSON.stringify(datos),
+
+                header).toPromise().then(
+
+                    (data: any) => { // Success
+
+                        let alumno: Alumno;
+
+                        alumno = Alumno.createFromJsonObject(data);
+
+                        resolve(alumno);
+
+                    }
+
+                )
+
+                .catch((error: Error) => {
+
+                    reject(error.message);
+                });
+
+        });
+
+        return promise;
+
+    }
+
+}
